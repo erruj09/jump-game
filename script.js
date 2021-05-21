03820aaf918a096ad874b4aa30b2501b537f0a77
erruj09 <82642042+erruj09@users.noreply.github.com>
2021-05-21 08:54:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-var character =
-document.getElementById("character");
+var character =document.getElementById("character");
 var block = document.getElementById("block");
 function jump(){
-    character.classList.add("animate");
+    if(character.classList != "animate"){
+        character.classList.add("animate");
+    }    
+    setTimeout(function(){
+        character.classList.remove("animate")
+    },500);
 }
+
+var chechDead = setInterval(function(){
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>0 %% charactertop>=130){
+        block.style.animation = "none"
+        block.style.display = "none"
+        Alert("u lose");
+    }
+},10)
